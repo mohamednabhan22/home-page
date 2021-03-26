99f1ec9674106bcab7a3a4342704566794551246
mohamed <mohamednabhan2280@gmail.com>
2021-03-26 16:44:41 +0200

    home-page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+
+
+    $(".navbar li a").click(function (e) {
+
+        e.preventDefault(); 
+        
+
+$(this).parent().addClass("active").siblings().removeClass("active")
+
+        
+
+        
+
+        
+
+
+
+    });
+});
